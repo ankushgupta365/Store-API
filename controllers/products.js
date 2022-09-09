19d75a8eced00b95cfc3c1567b59a8d5a659b5ee
Ankush Gupta <ankushgupta365@gmail.com>
2022-09-10 01:25:56 +0530

    fetch products according to query parameters implemented

--- controllers/products.js
+++ controllers/products.js
@@ -1,8 +1,23 @@
+const ProductModel = require('../models/product')
 const getAllProdutsStatic = async (req,res) =>{
-    res.status(200).json({msg: 'products static received'})
+    const products = await ProductModel.find({})
+    res.status(200).json({products,noHits: products.length})
 }
 const getAllProducts= async (req,res) =>{
-    res.status(200).json({msg: 'products received'})
+    const {featured,name,company} = req.query
+    const querySelector = {}
+    if(featured){
+        querySelector.featured = featured === 'true'?true: false
+    }
+    if(name){
+        //below regex is for case insensitive search with query with key name
+        querySelector.name = {$regex: name, $options: 'i'}
+    }
+    if(company){
+        querySelector.company = company
+    }
+    const products = await ProductModel.find(querySelector)
+    res.status(200).json({products, noHits: products.length})
 }
 module.exports = {
     getAllProducts,
