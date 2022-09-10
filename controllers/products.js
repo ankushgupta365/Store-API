d960203417373a213827effca6ec3e5ddb9c4291
Ankush Gupta <ankushgupta365@gmail.com>
2022-09-10 15:21:47 +0530

    pagination and limit added, with addition of useful comments

--- controllers/products.js
+++ controllers/products.js
@@ -20,14 +20,22 @@ const getAllProducts= async (req,res) =>{
     let results =  ProductModel.find(querySelector)
     if(sort){
         const sortList = sort.split(',').join(' ')
-        results.sort(sortList)
+        results = results.sort(sortList)
     }else{
-        results.sort('createdAt')
+        results =results.sort('createdAt')
     }
     if(fields){
         const fieldsList = fields.split(',').join(' ')
-        results.select(fieldsList)
+        results = results.select(fieldsList)
     }
+
+    // pagination and limit
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10  //by default 10 is the limit of objects to fetch
+    const skip = (page - 1) * limit       // if limit is 10 and page is 2 then, skip will be 10, so it will show the objects after skipping 10 objects from the results
+
+    results = results.skip(skip).limit(limit)
+
     const products = await results
     res.status(200).json({products, noHits: products.length})
 }
