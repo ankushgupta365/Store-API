6d6753508c2b0ddc5233333c16c4fde6f6b49917
Ankush Gupta <ankushgupta365@gmail.com>
2022-09-10 19:26:46 +0530

    numeric filtering for price and rating added

--- controllers/products.js
+++ controllers/products.js
@@ -1,32 +1,55 @@
 const ProductModel = require('../models/product')
-const getAllProdutsStatic = async (req,res) =>{
+const getAllProdutsStatic = async (req, res) => {
     const products = await ProductModel.find({})
-    res.status(200).json({products,noHits: products.length})
+    res.status(200).json({ products, noHits: products.length })
 }
-const getAllProducts= async (req,res) =>{
-    const {featured,name,company, sort, fields} = req.query
+const getAllProducts = async (req, res) => {
+    const { featured, name, company, sort, fields, numericFilters } = req.query
     const querySelector = {}
-    if(featured){
-        querySelector.featured = featured === 'true'?true: false
+    if (featured) {
+        querySelector.featured = featured === 'true' ? true : false
     }
-    if(name){
+    if (name) {
         //below regex is for case insensitive search with query with key name
-        querySelector.name = {$regex: name, $options: 'i'}
+        querySelector.name = { $regex: name, $options: 'i' }
     }
-    if(company){
+    if (company) {
         querySelector.company = company
     }
+    // to add numericfilters to the queryselector according to the mongoose requirement
+    if (numericFilters) {
+        const operatorMap = {
+            '>': '$gt',
+            '>=': '$gte',
+            '=': '$eq',
+            '<': '$lt',
+            '<=': '$lte'
+        }
+        const regex = /\b(<|>|>=|=|<|<=)\b/g;  //regex for all the possible numeric filter values pattern
+        let filters = numericFilters.replace(regex, (match) => `-${operatorMap[match]}-`)      // match will get replaced with the values from operatorMap
 
-    let results =  ProductModel.find(querySelector)
-    if(sort){
+        const options = ['price', 'rating']  //these are the only fields where numeric filters will be applied
+
+        filters = filters.split(',').forEach(element => {
+            const [field, operator, value] = element.split('-') // destructuring the array bcz split method return the array separted by the passed separator
+            if (options.includes(field)) {
+                querySelector[field] = { [operator]: Number(value) }
+            }
+        });
+    }
+    console.log(querySelector)
+    let results = ProductModel.find(querySelector)
+
+    //sorting and field selecting
+    if (sort) {
         const sortList = sort.split(',').join(' ')
         results = results.sort(sortList)
-    }else{
-        results =results.sort('createdAt')
+    } else {
+        results = results.sort('createdAt')
     }
-    if(fields){
+    if (fields) {
         const fieldsList = fields.split(',').join(' ')
-        results = results.select(fieldsList)
+        results = results.select(fieldsList)   // using select command only fiels passed in query parameters get selected
     }
 
     // pagination and limit
@@ -37,7 +60,7 @@ const getAllProducts= async (req,res) =>{
     results = results.skip(skip).limit(limit)
 
     const products = await results
-    res.status(200).json({products, noHits: products.length})
+    res.status(200).json({ products, noHits: products.length })
 }
 module.exports = {
     getAllProducts,
